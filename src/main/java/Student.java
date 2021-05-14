b4af07984afb9fbf84904ba8ef8e3ba3226d4450
Roland Valdez <valdez.roland.23@gmail.com>
2021-05-14 12:23:34 -0500

    completely refactored

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,7 @@
+
+
+
+
 import java.util.ArrayList;
 
 public class Student {
@@ -5,47 +9,26 @@ public class Student {
     private long id;
     private ArrayList<Integer> grades;
 
-    public Student(String name, long id){
-        ArrayList<Integer> grades = new ArrayList<>();
-        this.name = name;
+    public Student(long id, String name) {
+        ArrayList<Integer> grades = new ArrayList<Integer>();
         this.id = id;
+        this.name = name;
         this.grades = grades;
-    }
-    public static void main(String[] args) {
-        Student roland = new Student("roland", 12345);
-        System.out.println("roland.getName() = " + roland.getName());
-        System.out.println("roland.getId() = " + roland.getId());
-        roland.addGrade(50);
-        roland.addGrade(100);
-        System.out.println("roland.getGrades() = " + roland.getGrades());
-        System.out.println("roland.getGradeAverage() = " + roland.getGradeAverage());
 
     }
-    // returns the student's id
-    public long getId(){
+
+    public long getId() {
         return this.id;
     }
 
-    // returns the student's name
-    public String getName(){
+    public String getName() {
         return this.name;
     }
+    public ArrayList<Integer> getGrades(){
+        return grades;
+    }
 
-    // adds the given grade to the grades list
     public void addGrade(int grade) {
         grades.add(grade);
-    }
-    // returns the list of grades
-    public ArrayList<Integer> getGrades(){
-        return this.grades;
-    }
-
-    // returns the average of the students grades
-    public double getGradeAverage(){
-        int total = 0;
-        for (Integer grade: grades){
-            total += grade;
-        }
-        return (double) total / grades.size();
     }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -6,32 +6,30 @@ import static org.junit.Assert.*;
 public class StudentTest {
 
     @Test
-    public void isLongNumber(){
-        long number = 500;
-        assertTrue(Long.class.isInstance(number));
+    public void constructorValid(){
+        Student roland = new Student(1L, "roland");
+        assertNotNull(roland);
+        roland = null;
+        assertNull(roland);
 
     }
 
-//    public void isUnique(){
-//        for
-//        assertTrue();
-//    }
     @Test
-    public void String(){
-        String word = "";
-        assertTrue(String.class.isInstance(word));
-        assertFalse(String.class.isInstance(null));
-        assertTrue(String.class.isInstance(""));
-        assertFalse(String.class.isInstance(15));
-    }
-    @Test
-    public void isGradesInt(){
-        ArrayList<Integer> grades = new ArrayList<>();
-        grades.add(50);
-        grades.add(11);
-        for (Integer grade : grades){
-            assertTrue(Integer.class.isInstance(grade));
-        }
+    public void constructorHasSetProperties(){
+        Student roland = new Student(1L, "roland");
+        assertEquals(1, roland.getId());
+        assertNotEquals(2,roland.getId());
+        assertEquals("roland", roland.getName());
+        assertNotEquals("norma",roland.getName());
     }
 
+    @Test
+    public void isArrayListEmpty(){
+        Student roland = new Student(1L, "roland");
+
+        assertTrue(roland.getGrades().isEmpty());
+        roland.addGrade(100);
+        assertFalse(roland.getGrades().isEmpty());
+
+    }
 }
