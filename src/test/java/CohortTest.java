0a60da0dfc5e22d73dd4cae853379026c8c882cf
Roland Valdez <valdez.roland.23@gmail.com>
2021-05-17 10:50:54 -0500

    had to restart CohortTest due to rebase

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -1,21 +1,43 @@
 
+
+import org.junit.Before;
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 import static org.junit.Assert.*;
-public class CohortTest {
 
+public class CohortTest {
+    Cohort singleStudent;
+    Cohort twoStudents;
+
+    @Before
+    public void start(){
+        singleStudent = new Cohort();
+        twoStudents = new Cohort();
+        Student roland = new Student(1L, "roland");
+        Student norma = new Student(5L, "norma");
+        roland.addGrade(100);
+        norma.addGrade(50);
+        singleStudent.addStudent(roland);
+        twoStudents.addStudent(roland);
+        twoStudents.addStudent(norma);
+    }
 
     @Test
-    public void testAddToListStudent() {
-        Student roland = new Student(1L, "roland");
-        List<Student> students = new ArrayList<Student>();
-        assertTrue(students.isEmpty());
-        students.add(roland);
-//        Cohort.addStudent(roland);
-//        students.addStudent(roland);
-//        assertEquals(1, students.size());
-
+    public void testCanAddStudents(){
+        assertEquals(1, singleStudent.getStudents().size());
+        assertEquals(2, twoStudents.getStudents().size());
     }
+
+    @Test
+    public void testCanGetStudent(){
+        assertEquals("roland", singleStudent.getStudents().get(0).getName());
+        assertEquals("norma", twoStudents.getStudents().get(1).getName());
+    }
+
+    @Test
+    public void testAverageWorks(){
+        assertEquals(100, singleStudent.getCohortAverage(), 0);
+        assertEquals(75, twoStudents.getCohortAverage(), 0);
+    }
+
+
 }
